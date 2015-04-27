1905b070569b5df3c80048370761e017cf90a1ff
Marco Barbosa <marcobarbosa@iclio.pt>
2015-04-27 23:32:36 +0100

    INITIAL COMMIT;

---
+++ TP_PA/src/Dices/Dice.java
@@ -0,0 +1,16 @@
+package Dices;
+
+/**
+ *
+ * @author marcobarbosa
+ */
+public class Dice {
+    
+    private final int id;
+    private int face;
+
+    public Dice(int id, int face) {
+        this.id = id;
+        this.face = face;
+    }
+}
---
+++ TP_PA/src/tp_pa/TP_PA.java
@@ -0,0 +1,16 @@
+package tp_pa;
+
+/**
+ *
+ * @author marcobarbosa
+ */
+public class TP_PA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
