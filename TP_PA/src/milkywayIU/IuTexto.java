25295e01aa5e8abe1b61c1cf426bd2beddda958e
Marco Barbosa <marcobarbosa@iclio.pt>
2015-05-04 23:28:50 +0100

    Text UI added;

--- TP_PA/src/milkywayIU/IuTexto.java
+++ TP_PA/src/milkywayIU/IuTexto.java
@@ -1,17 +1,50 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package milkywayIU;
 
+import java.util.Scanner;
 import milkyway_logic.gameplanner.Game;
 
-/**
- *
- * @author Diogo
- */
 public class IuTexto {
-    Game game;
     
+    private Game game;
+    private boolean isExit;
+
+    public IuTexto() {
+        this.isExit = false;
+    }
+    
+  
+    public void start(){
+        
+        printMenu();
+        while(!isExit){
+            
+            System.out.println(" Option:");
+            Scanner mScanner = new Scanner(System.in);
+            int option = mScanner.nextInt();
+            
+            switch(option){
+                case 1: //startGame();
+                    break;
+                case 2: //vercenas
+                
+                case 3: //consultar
+                
+                case 4: System.out.println(" Thank's for being with us, mate!");
+                        isExit = true;
+                    return;
+                        
+            }
+            
+        }
+        
+    }
+    
+    private void printMenu(){
+        
+        System.out.println(" :::::::::  MILKY WAY EXPRESS  ::::::::::::");
+        System.out.println(" 1. Iniciar Jogo");
+        System.out.println(" 2. ver cenas");
+        System.out.println(" 3. Consultar");
+        System.out.println(" 4. Sair");
+    }
 }
--- TP_PA/src/milkywayIU/MilkyWay.java
+++ TP_PA/src/milkywayIU/MilkyWay.java
@@ -1,18 +1,12 @@
 package milkywayIU;
 
-
-
 import milkyway_logic.gameplanner.Game;
 
-/**
- *
- * @author marcobarbosa
- */
 public class MilkyWay {
 
     public static void main(String[] args) {
         
-        Game mGame = new Game();
-        mGame.newGame();
+        IuTexto mIuTexto = new IuTexto();
+        mIuTexto.start();
     }
 }
