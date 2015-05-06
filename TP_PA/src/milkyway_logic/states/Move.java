c00e4e00fe42fbe3f7bd7514efe7f269f2b004c7
Marco Barbosa <marcobarbosa@iclio.pt>
2015-05-06 02:06:57 +0100

    board code replaced. It disappeared after merge conflict

--- TP_PA/src/milkyway_logic/states/Move.java
+++ TP_PA/src/milkyway_logic/states/Move.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package milkyway_logic.states;
 
 import milkyway_logic.gameplanner.Game;
 
-/**
- *
- * @author Diogo
- */
 public class Move extends State{
 
     public Move(Game game) {
