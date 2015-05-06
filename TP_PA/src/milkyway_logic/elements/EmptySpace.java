89344ff27bdf2e41ea1e322736196bba22b75d1e
Marco Barbosa <marcobarbosa@iclio.pt>
2015-05-06 19:25:15 +0100

    interface implementation on these methods;

--- TP_PA/src/milkyway_logic/elements/EmptySpace.java
+++ TP_PA/src/milkyway_logic/elements/EmptySpace.java
@@ -1,5 +1,8 @@
 package milkyway_logic.elements;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class EmptySpace extends Card {
 
     private boolean isTurned;
@@ -8,6 +11,36 @@ public class EmptySpace extends Card {
         super();
 
         this.isTurned = false;
+    }
+
+    @Override
+    public List<Cube> getCubeList() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setCubeList(List<Cube> cubeList) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public HashMap<String, Integer> getPrices() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setPrices(HashMap<String, Integer> prices) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getPlanetName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setPlanetName(String planetName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
--- TP_PA/src/milkyway_logic/elements/Wormhole.java
+++ TP_PA/src/milkyway_logic/elements/Wormhole.java
@@ -1,9 +1,42 @@
 package milkyway_logic.elements;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class Wormhole  extends Card{
 
     public Wormhole() {
         super();
+    }
+
+    @Override
+    public List<Cube> getCubeList() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setCubeList(List<Cube> cubeList) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public HashMap<String, Integer> getPrices() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setPrices(HashMap<String, Integer> prices) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getPlanetName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setPlanetName(String planetName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
