525a71cda2b3ccdde817e3c5e3ae7386b19d8985
Diogo Nunes <diogonuuunes@gmail.com>
2015-05-10 00:39:20 +0100

    Sell state fully implemented!

--- TP_PA/src/milkyway_logic/states/Sell.java
+++ TP_PA/src/milkyway_logic/states/Sell.java
@@ -23,33 +23,57 @@ public class Sell extends State {
     }
 
     @Override
-    public State sellCargo(String carga) {
-        if (getGame().getPlayer().getSpaceship().getCargo().isEmpty()) {
-            return new Buy(getGame());
-        } else if (getGame().getBoard()[getGame().getPlayer().getSpaceship().getPosX()][getGame().getPlayer().getSpaceship().getPosY()] instanceof Planet) {
+    public State sellCargo(String cargo) {
+        int PosX = getGame().getPlayer().getSpaceship().getPosX();
+        int PosY = getGame().getPlayer().getSpaceship().getPosY();
+
+        if (getGame().getBoard()[PosX][PosY] instanceof Planet) {
             //If spaceship is on planet ortherwise it can't sell
 
             int cargoPrice;
-            HashMap<String, Integer> prices = getGame().getBoard()[getGame().getPlayer().getSpaceship().getPosX()][getGame().getPlayer().getSpaceship().getPosY()].getPrices();
-            List<Cube> cubeListPlanet = getGame().getBoard()[getGame().getPlayer().getSpaceship().getPosX()][getGame().getPlayer().getSpaceship().getPosY()].getCubeList();
+            HashMap<String, Integer> prices = getGame().getBoard()[PosX][PosY].getPrices();
+            List<Cube> cubeListPlanet = getGame().getBoard()[PosX][PosY].getCubeList();
+            System.out.println("AZUL PREÇO -> " + prices.get("blue"));
+            //^FETCHES CUBE LIST PLANET
 
-            if (!getGame().getPlayer().getSpaceship().getCargo().isEmpty()) {
+            List<Cube> cubesSpaceship = getGame().getPlayer().getSpaceship().getCargo();
+            cargoPrice = prices.get(cargo);
+            if (getGame().getBoard()[PosX][PosY].isPirate()) {
+                getGame().getPlayer().setCoins(getGame().getPlayer().getCoins() + prices.get(cargo));
 
-                List<Cube> cubesSpaceship = getGame().getPlayer().getSpaceship().getCargo();
-                cargoPrice = prices.get(carga);
-                if (cubeListPlanet.get(0).getColor().equals(carga) && cubeListPlanet.get(1).getColor().equals(carga)) {
-                    getGame().getPlayer().setCoins(+prices.get(carga));
-                    cubesSpaceship.remove(new Cube(carga));
-                    getGame().getPlayer().getSpaceship().setCargo(cubesSpaceship);
-                } else if ((!cubeListPlanet.get(0).getColor().equals(carga) && cubeListPlanet.get(1).getColor().equals(carga)) || (cubeListPlanet.get(0).getColor().equals(carga) && !cubeListPlanet.get(1).getColor().equals(carga))) {
-                    getGame().getPlayer().setCoins(getGame().getPlayer().getCoins() + prices.get(carga) + 1);
-                    cubesSpaceship.remove(new Cube(carga));
-                    getGame().getPlayer().getSpaceship().setCargo(cubesSpaceship);
-                } else {
-                    getGame().getPlayer().setCoins(getGame().getPlayer().getCoins() + prices.get(carga) + 2);
-                    cubesSpaceship.remove(new Cube(carga));
-                    getGame().getPlayer().getSpaceship().setCargo(cubesSpaceship);
+                int pos = getCubePos(cubesSpaceship, cargo);
+                if (pos < 99) {
+                    cubesSpaceship.remove(pos);
                 }
+                getGame().getPlayer().getSpaceship().setCargo(cubesSpaceship);
+                
+                return this;
+            }
+            
+            if (cubeListPlanet.get(0).getColor().compareTo(cargo) == 0 && cubeListPlanet.get(1).getColor().compareTo(cargo) == 0) {
+                getGame().getPlayer().setCoins(getGame().getPlayer().getCoins() + prices.get(cargo));
+
+                int pos = getCubePos(cubesSpaceship, cargo);
+                if (pos < 99) {
+                    cubesSpaceship.remove(pos);
+                }
+                getGame().getPlayer().getSpaceship().setCargo(cubesSpaceship);
+            } else if ((!cubeListPlanet.get(0).getColor().equals(cargo) && cubeListPlanet.get(1).getColor().equals(cargo)) || (cubeListPlanet.get(0).getColor().equals(cargo) && !cubeListPlanet.get(1).getColor().equals(cargo))) {
+                getGame().getPlayer().setCoins(getGame().getPlayer().getCoins() + prices.get(cargo) + 1);
+
+                int pos = getCubePos(cubesSpaceship, cargo);
+                if (pos < 99) {
+                    cubesSpaceship.remove(pos);
+                }
+
+                getGame().getPlayer().getSpaceship().setCargo(cubesSpaceship);
+            } else {
+                getGame().getPlayer().setCoins(getGame().getPlayer().getCoins() + prices.get(cargo) + 2);
+                int pos = getCubePos(cubesSpaceship, cargo);
+                if (pos < 99) {
+                    cubesSpaceship.remove(pos);
+                }
+                getGame().getPlayer().getSpaceship().setCargo(cubesSpaceship);
             }
 
         }
@@ -66,8 +90,8 @@ public class Sell extends State {
     public State upgradeWeapon() {
 
         if (getGame().getPlayer().getSpaceship().getPower() < 6 && getGame().getPlayer().getCoins() >= 4) {
-            getGame().getPlayer().getSpaceship().setPower(getGame().getPlayer().getSpaceship().getPower()+1);
-            getGame().getPlayer().setCoins(getGame().getPlayer().getCoins()-4);
+            getGame().getPlayer().getSpaceship().setPower(getGame().getPlayer().getSpaceship().getPower() + 1);
+            getGame().getPlayer().setCoins(getGame().getPlayer().getCoins() - 4);
         }
         return this;
     }
@@ -75,8 +99,8 @@ public class Sell extends State {
     @Override
     public State upgradeCargo() {
         if (getGame().getPlayer().getSpaceship().getCargo().size() == 2) {
-            getGame().getPlayer().getSpaceship().setCapacity(getGame().getPlayer().getSpaceship().getCapacity()+1);
-            getGame().getPlayer().setCoins(getGame().getPlayer().getCoins()-4);
+            getGame().getPlayer().getSpaceship().setCapacity(getGame().getPlayer().getSpaceship().getCapacity() + 1);
+            getGame().getPlayer().setCoins(getGame().getPlayer().getCoins() - 4);
         }
         return this;
     }
@@ -89,5 +113,14 @@ public class Sell extends State {
     @Override
     public State nextState() {
         return new Buy(getGame());
+    }
+
+    private int getCubePos(List<Cube> c, String cargo) {
+        for (int i = 0; i < c.size(); i++) {
+            if (c.get(i).getColor().compareTo(cargo) == 0) {
+                return i;
+            }
+        }
+        return 99;
     }
 }
