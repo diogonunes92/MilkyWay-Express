4ccec376e5227912a5afa2f6464591c001ddf315
Marco Barbosa <marcobarbosa@iclio.pt>
2015-05-26 19:31:00 +0100

    paintComponent added

--- TP_PA/src/milkywayGIU/View/GameBoard.java
+++ TP_PA/src/milkywayGIU/View/GameBoard.java
@@ -14,6 +14,7 @@ public final class GameBoard extends JPanel {
     GameBoard(Model model) {
         this.model = model;
         this.model.constructGame();
+        this.paintComponent(getGraphics());
     }
 
     void registerObservers() {
