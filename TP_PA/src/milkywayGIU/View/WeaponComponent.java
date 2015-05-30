419d4bf6f1a1b806026216c2b4fadeac78f66128
Marco António Santos Barbosa <marco.de.barbosa@gmail.com>
2015-05-30 04:59:40 +0100

    Weapon component implemented

--- TP_PA/src/milkywayGIU/View/WeaponComponent.java
+++ TP_PA/src/milkywayGIU/View/WeaponComponent.java
@@ -1,14 +1,22 @@
 package milkywayGIU.View;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -18,10 +26,10 @@ import util.Constants;
 
 public class WeaponComponent extends JPanel implements Observer {
 
-    private Model model;
+    private final Model model;
     private JLabel titleLabel, weaponPowerLabel;
     private JButton weaponUpgrade1Button, weaponUpgrade2Button;
-    private JPanel panel;
+    private String backgroundImage;
 
     public WeaponComponent(Model model) {
         this.model = model;
@@ -33,17 +41,69 @@ public class WeaponComponent extends JPanel implements Observer {
 
         setupComponents();
         setupLayout();
+        registerListeners();
     }
 
     private void setupComponents() {
 
-        this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-        panel = new JPanel();
         titleLabel = new JLabel();
         weaponPowerLabel = new JLabel();
         weaponUpgrade1Button = new JButton();
         weaponUpgrade2Button = new JButton();
+    }
+
+    private void setupLayout() {
+
+        titleLabel.setText("WEAPON POWER");
+        titleLabel.setFont(Constants.FONT_16);
+        titleLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        weaponPowerLabel.setText(String.valueOf(this.model.getPlayer().getSpaceship().getPower()));
+        weaponPowerLabel.setFont(Constants.FONT_25);
+        weaponPowerLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        weaponUpgrade1Button.setBackground(Color.LIGHT_GRAY);
+        weaponUpgrade1Button.setText("Upgrade Weapon 1");
+        weaponUpgrade1Button.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        weaponUpgrade2Button.setBackground(Color.LIGHT_GRAY);
+        weaponUpgrade2Button.setText("Upgrade Weapon 2");
+        weaponUpgrade2Button.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        this.add(titleLabel);
+        this.add(Box.createRigidArea(new Dimension(0, Constants.INSIDE_PANEL_SPACE)));
+        this.add(weaponPowerLabel);
+        this.add(Box.createRigidArea(new Dimension(0, Constants.INSIDE_PANEL_SPACE)));
+        this.add(weaponUpgrade1Button);
+        this.add(Box.createRigidArea(new Dimension(0, Constants.INSIDE_PANEL_SPACE)));
+        this.add(weaponUpgrade2Button);
+
+    }
+
+    void registerObservers() {
+        model.addObserver(this);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        try {
+            g.drawImage(getBackgroundImage(), 0, 0, null);
+        } catch (IOException ex) {
+            Logger.getLogger(WeaponComponent.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private Image getBackgroundImage() throws IOException {
+
+        Image img = ImageIO.read(getClass().getResource("/images/weapon_panel.jpg"));
+        img.flush();
+        return img;
+    }
+
+    private void registerListeners() {
 
         weaponUpgrade1Button.addMouseListener(new MouseAdapter() {
 
@@ -60,35 +120,6 @@ public class WeaponComponent extends JPanel implements Observer {
                 model.upgradeWeapon();
             }
         });
-    }
-
-    private void setupLayout() {
-
-        panel.setLayout(new GridLayout(4, 1));
-        panel.setBackground(Color.orange);
-
-        titleLabel.setText("WEAPON POWER");
-        titleLabel.setFont(Constants.FONT_13);
-
-        weaponUpgrade1Button.setBackground(Color.red);
-        weaponUpgrade1Button.setText("Upgrade Weapon 1");
-
-        weaponUpgrade2Button.setBackground(Color.red);
-        weaponUpgrade2Button.setText("Upgrade Weapon 2");
-
-        weaponPowerLabel.setText(String.valueOf(this.model.getPlayer().getSpaceship().getPower()));
-        weaponPowerLabel.setFont(Constants.FONT_25);
-
-        panel.add(titleLabel);
-        panel.add(weaponPowerLabel);
-        panel.add(weaponUpgrade1Button);
-        panel.add(weaponUpgrade2Button);
-
-        this.add(panel);
-    }
-
-    void registerObservers() {
-        model.addObserver(this);
     }
 
     @Override
